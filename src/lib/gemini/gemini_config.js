d98b469f7189ce5aae8f3654716691415326f5fa
yindiii <yindizhao666@gmail.com>
2025-02-19 13:40:19 -0600

    removed js files and redeployed

--- src/lib/gemini/gemini_config.js
+++
@@ -1,4 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-const gemini_key = import.meta.env.VITE_APP_GEMINI_API_KEY;
-export const GeminiObject = new GoogleGenerativeAI(gemini_key);
-export const Gemini_2_0 = GeminiObject.getGenerativeModel({ model: "gemini-2.0-flash" });
