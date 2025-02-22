462318b02b392186dac35ae1e584f6f918acbca6
chip <49256163+chipanyanwu@users.noreply.github.com>
2025-02-21 18:20:24 -0600

    first message system prompt bug fix

--- src/lib/gemini/gemini_config.ts
+++ src/lib/gemini/gemini_config.ts
@@ -1,7 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const gemini_key = import.meta.env.VITE_GEMINI_API_KEY
-console.log(`GEMINI KEY : ${gemini_key}`);
 
 export const GeminiObject = new GoogleGenerativeAI(gemini_key)
 export const Gemini_2_0 = GeminiObject.getGenerativeModel({
