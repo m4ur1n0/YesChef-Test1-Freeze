940086dc4fc0aa6be28b2e201c409970a9bb801f
theo <thjmaurino@gmail.com>
2025-02-24 22:38:18 -0600

    prepared for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import App from './App'
 
+require('dotenv').config();
+// console.log(process.env.API_KEY);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
