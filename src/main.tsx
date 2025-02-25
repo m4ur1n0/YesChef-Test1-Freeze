bb9c7d52fd239223c28d15b0dd1ed9ffbf89c597
theo <thjmaurino@gmail.com>
2025-02-24 22:48:24 -0600

    overwrite dead api key :(

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import App from './App'
 
-require('dotenv').config();
-// console.log(process.env.API_KEY);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
